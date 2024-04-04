83c75124d4f2803b3d620cb4fa4a1d22fd0bac48
naixatwork <naixatwork@gmail.com>
2024-04-04 14:32:10 +0330

    feat(layout): implemented layout

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
 
-    // const routes = {
-    //     '/': Counter,
-    //     '/auth': Dastan,
-    // }
     import {AppBar, AppShell} from "@skeletonlabs/skeleton";
 </script>
 
-<main>
-    <AppShell>
-        <svelte:fragment slot="header">
-            <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-                <svelte:fragment slot="lead">(icon)</svelte:fragment>
-                (title)
-                <svelte:fragment slot="trail">(actions)</svelte:fragment>
-            </AppBar>
-        </svelte:fragment>
-        <h1>lol</h1>
-    </AppShell>
-</main>
+<AppShell>
+    <svelte:fragment slot="header">
+        <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+            <svelte:fragment slot="lead">
+                <div class="flex gap-2">
+                    <img class="aspect-square w-5" src="https://cryptologos.cc/logos/solana-sol-logo.svg?v=029"
+                         alt="solana"/>
+                    <p class="font-bold">solana - dns</p>
+                </div>
+            </svelte:fragment>
+            <svelte:fragment slot="trail">wallet</svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+    <div class="p-4 overflow-scroll h-full">
+        <h1>content</h1>
+    </div>
+</AppShell>
