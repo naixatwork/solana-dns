36d332c47cfb1d4269265d7be93df1a3aaf271b6
naixatwork <naixatwork@gmail.com>
2024-04-05 12:32:52 +0330

    feat(wallet): initialized wallet connection

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
-
     import {AppBar, AppShell} from "@skeletonlabs/skeleton";
+    import {WalletMultiButton} from '@svelte-on-solana/wallet-adapter-ui'
+    import WalletConnection from "../core/wallet/WalletInit.svelte";
 </script>
 
+<WalletConnection/>
 <AppShell>
     <svelte:fragment slot="header">
         <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
             <svelte:fragment slot="lead">
-                <div class="flex gap-2">
-                    <img class="aspect-square w-5" src="https://cryptologos.cc/logos/solana-sol-logo.svg?v=029"
+                <div class="flex gap-5 items-center">
+                    <img class="aspect-square w-10" src="https://cryptologos.cc/logos/solana-sol-logo.svg?v=029"
                          alt="solana"/>
-                    <p class="font-bold">solana - dns</p>
+                    <p class="font-bold text-2xl">solana - dns</p>
                 </div>
             </svelte:fragment>
-            <svelte:fragment slot="trail">wallet</svelte:fragment>
+            <svelte:fragment slot="trail">
+                <WalletMultiButton/>
+            </svelte:fragment>
         </AppBar>
     </svelte:fragment>
-    <div class="p-4 overflow-scroll h-full">
+    <div class="p-4 h-full">
         <h1>content</h1>
     </div>
 </AppShell>
---
+++ src/core/wallet/WalletInit.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import {ConnectionProvider, WalletProvider} from '@svelte-on-solana/wallet-adapter-ui'
+    import {clusterApiUrl} from '@solana/web3.js'
+    import {onMount} from "svelte";
+    import type {Adapter} from "@solana/wallet-adapter-base";
+
+    const network = clusterApiUrl('devnet')
+    const localStorageKey = 'solWalletAdapter'
+    let wallets: Adapter[] = []
+    onMount(async () => {
+        const {PhantomWalletAdapter} = await import('@solana/wallet-adapter-wallets')
+        wallets = [new PhantomWalletAdapter()]
+    })
+</script>
+
+<WalletProvider {localStorageKey} {wallets} autoConnect/>
+<ConnectionProvider {network}/>
